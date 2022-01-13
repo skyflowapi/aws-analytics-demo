4bf2927d57da0d427ea4beda8ad429c640ff18e0
Sean Falconer <67345830+thefalc@users.noreply.github.com>
2022-01-13 14:18:44 -0800

    Fix for auth bearer token generation

--- dynamodb-processor-lambda/awsUtil.js
+++ dynamodb-processor-lambda/awsUtil.js
@@ -1,4 +1,7 @@
 const AWS = require('aws-sdk');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
 const {
     REGION,
     SECRET_NAME,
@@ -6,14 +9,55 @@ const {
 } = require('./config');
 
 async function getVaultBearerToken() {
-
     // Create a Secrets Manager client
     const client = new AWS.SecretsManager({ region: REGION });
 
     const data = await client.getSecretValue({ SecretId: SECRET_NAME }).promise();
-    const jsonData = JSON.parse(data.SecretString);
-    
-    return jsonData[SECRET_KEY];
+    console.log('raw secret data: ' + data.SecretString);
+    const rawCredential = JSON.parse(data.SecretString);
+
+    // Return the bearer token needed for Skyflow API calls
+    return await getBearerFromRawCredentials(rawCredential[SECRET_KEY]);
+}
+
+function getSignedJWT(rawCredentials) {
+  const creds = JSON.parse(rawCredentials);
+
+  const claims = {
+    iss: creds["clientID"],
+    key: creds["keyID"], 
+    aud: creds["tokenURI"], 
+    exp: Math.floor(Date.now() / 1000) + (60 * 60), //JWT expires in Now + 60 minutes
+    sub: creds["clientID"]
+  };
+
+  const signedJWT = jwt.sign(claims, creds["privateKey"], { algorithm: 'RS256' });
+
+  return { signedJWT, creds };
+}
+
+async function getBearerToken(signedJWT, creds) {
+  const body = {
+    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+    assertion: signedJWT
+  }
+
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+  const tokenURI = creds["tokenURI"];
+  const response = await axios.post(tokenURI, body, { headers: headers });
+
+  return await response.data.accessToken;
+}
+
+async function getBearerFromRawCredentials(rawCredential) {
+  const { signedJWT, creds } = getSignedJWT(rawCredential);
+  const bearer = await getBearerToken(signedJWT, creds);
+
+  return bearer;
 }
 
 module.exports = getVaultBearerToken;
