3489d047feaba470290217953acc7f8c77070048
Sean Falconer <67345830+thefalc@users.noreply.github.com>
2022-01-15 12:21:09 -0800

    Added documentation and standardized code styles

--- kafka-processor-lambda/awsUtil.js
+++ kafka-processor-lambda/awsUtil.js
@@ -3,15 +3,18 @@ const AWS = require('aws-sdk');
 const region = process.env.REGION;
 const secretName = process.env.SECRET_NAME;
 
+/**
+ * Gets the database connection credentials from the AWS Secrets Manager.
+ * @returns JSON data representing the database connection credentials.
+ */
 async function getDBCredentials() {
+  // Create a Secrets Manager client
+  const client = new AWS.SecretsManager({region: region});
 
-    // Create a Secrets Manager client
-    const client = new AWS.SecretsManager({ region: region });
+  const data = await client.getSecretValue({SecretId: secretName}).promise();
+  const jsonData = JSON.parse(data.SecretString);
 
-    const data = await client.getSecretValue({ SecretId: secretName }).promise();
-    const jsonData = JSON.parse(data.SecretString);
-    
-    return jsonData;
+  return jsonData;
 }
 
 module.exports = getDBCredentials;
